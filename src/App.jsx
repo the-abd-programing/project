3e93accd2cbfd9eee999141721d31463120ec062
bjirefbui <karimovabdurahmon1133@gmail.comgit config --global user.email karimovabdurahmon1133@gmail.comgit log>
2023-04-18 18:07:25 +0500

    Navbar added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Head from './Head/Head.jsx'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <>
+      <Head />
+      </>
+    </div>
+  );
+}
+
+export default App;
